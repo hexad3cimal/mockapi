af28bb314dfcf40c0d10bc5bcdd430026809d10c
Jovin Thariyath <jovin.war@gmail.com>
2019-10-22 23:10:06 +0530

    add test cases

--- app/engine/v1/api/routes_test.go
+++ app/engine/v1/api/routes_test.go
@@ -1,20 +1,32 @@
 package api
 
 import (
-	"github.com/hexad3cimal/mockapi/app/engine/v1/models"
-	"github.com/hexad3cimal/mockapi/app/engine/v1/repository"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func ApiPostTest(t *testing.T) {
-var addApi = repository.AddApi
-addApiMock := addApi
+func TestApiPost(t *testing.T) {
 
-defer func() {  addApi = addApiMock }()
+	var jsonStr = []byte(`{"id":4,"url":"test","created_at":"","updated_at":"","active":1}`)
 
-	addApiMock = func(api models.Api) string{
-		return "success"
+	req, err := http.NewRequest("POST", "/api", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ApiPost)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
 
-}
+	// Check the response body is what we expect.
+	if rr.Body.String() != "success" {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), "success")
+	}
 
 }
