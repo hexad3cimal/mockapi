0b0482d644f22490a4b8fcb4b9152bb2dfc84d10
Jovin Thariyath <jovin.war@gmail.com>
2019-10-30 22:02:37 +0530

    add enpoint wrapper

--- app/engine/v1/api/routes.go
+++ app/engine/v1/api/routes.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 )
 
-func PostApi(w http.ResponseWriter, r *http.Request) {
+func AddApi(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	w.Header().Add("Content-Type", "application/json")
 	if err != nil {
@@ -35,6 +35,47 @@ func PostApi(w http.ResponseWriter, r *http.Request) {
 		 w.Write(errorResponse)
 		 return
 	 }
+
+	w.WriteHeader(200)
+    successResponse, _ := json.Marshal(responses.Response{ResponseType: "success", Message:"success", Code: "200"})
+	if _, err := w.Write(successResponse); err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("unable to list response mapping"))
+	}
+}
+
+func AddEndpoint(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	w.Header().Add("Content-Type", "application/json")
+	if err != nil {
+		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(responses.Response{ResponseType: "error", Message: err.Error(), Code: "500"})
+		return
+	}
+
+	endPoint := models.EndPoint{}
+	jsonError := json.Unmarshal(body, &endPoint)
+	if jsonError != nil {
+		w.WriteHeader(400)
+		errorResponse, _ := json.Marshal(responses.Response{ResponseType: "error", Message:jsonError.Error(), Code: "400"})
+		w.Write(errorResponse)
+		return
+	}
+
+	api,err := repository.GetApi(endPoint.ApiId)
+	 if err != nil{
+	 	w.WriteHeader(400)
+		 errorResponse, _ := json.Marshal(responses.Response{ResponseType: "error", Message:"error occured", Code: "500"})
+		 w.Write(errorResponse)
+		 return
+	 }
+
+	if api != (models.Api{}){
+
+
+
+	}
 
 	w.WriteHeader(200)
     successResponse, _ := json.Marshal(responses.Response{ResponseType: "success", Message:"success", Code: "200"})
