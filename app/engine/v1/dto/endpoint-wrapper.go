6e84d7bf0b2d2508da246ff8257be45c1d433f28
Jovin Thariyath <jovin.war@gmail.com>
2019-11-15 22:58:31 +0530

    add wrapper

--- app/engine/v1/dto/endpoint-wrapper.go
+++ app/engine/v1/dto/endpoint-wrapper.go
@@ -1,1 +1,9 @@
 package dto
+
+import "github.com/hexad3cimal/mockapi/app/engine/v1/models"
+
+type EndpointWrapper struct{
+
+	ApiId string `json:"api_id"`
+	Endpoints[] models.EndPoint `json:"endpoints"`
+}
